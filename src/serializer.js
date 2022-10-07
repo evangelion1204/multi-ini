c6cfec72069d6bbc8d6a98c8b7062acac8077354
Michael Iwersen <michael.iwersen@zalando.de>
2022-10-07 19:30:51 +0200

    Fixed quoting to be applied only for strings

--- src/serializer.js
+++ src/serializer.js
@@ -8,7 +8,6 @@ const defaults = {
     keep_quotes: false,
 };
 
-
 class Serializer {
     constructor(options = {}) {
         this.options = Object.assign({}, defaults, options);
@@ -38,37 +37,50 @@ class Serializer {
     }
 
     serialize(content) {
-        return _.reduce(content, (output, sectionContent, section) => {
-            output += `[${section}]` + Constants.line_breaks[this.options.line_breaks];
-            output += this.serializeContent(sectionContent, '');
-            return output;
-        }, '');
+        return _.reduce(
+            content,
+            (output, sectionContent, section) => {
+                output += `[${section}]` + Constants.line_breaks[this.options.line_breaks];
+                output += this.serializeContent(sectionContent, '');
+                return output;
+            },
+            '',
+        );
     }
 
     serializeContent(content, path) {
-        return _.reduce(content, (serialized, subContent, key) => {
-            if (_.isArray(subContent)) {
-                for (let value of subContent) {
-                    if (this.needToBeQuoted(value)) {
-                        value = `"${value}"`;
+        return _.reduce(
+            content,
+            (serialized, subContent, key) => {
+                if (_.isArray(subContent)) {
+                    for (let value of subContent) {
+                        if (this.needToBeQuoted(value)) {
+                            value = `"${value}"`;
+                        }
+
+                        serialized += `${path}${path.length > 0 ? '.' : ''}${key}[]=${value}${
+                            Constants.line_breaks[this.options.line_breaks]
+                        }`;
+                    }
+                } else if (_.isObject(subContent)) {
+                    serialized += this.serializeContent(
+                        subContent,
+                        `${path}${path.length > 0 ? '.' : ''}${key}`,
+                    );
+                } else {
+                    if (_.isString(subContent) && this.needToBeQuoted(subContent)) {
+                        subContent = `"${subContent}"`;
                     }
 
-                    serialized += path + (path.length > 0 ? '.' : '') + key + "[]=" + value + Constants.line_breaks[this.options.line_breaks];
-                }
-            }
-            else if (_.isObject(subContent)) {
-                serialized += this.serializeContent(subContent, path + (path.length > 0 ? '.' : '') + key);
-            }
-            else {
-                if (this.needToBeQuoted(subContent)) {
-                    subContent = `"${subContent}"`;
+                    serialized += `${path}${path.length > 0 ? '.' : ''}${key}=${subContent}${
+                        Constants.line_breaks[this.options.line_breaks]
+                    }`;
                 }
 
-                serialized += path + (path.length > 0 ? '.' : '') + key + "=" + subContent + Constants.line_breaks[this.options.line_breaks];
-            }
-
-            return serialized;
-        }, '');
+                return serialized;
+            },
+            '',
+        );
     }
 }
 
